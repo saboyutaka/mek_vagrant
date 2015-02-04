85e4a3633c77ab41d4ab972987a2a861cd821a10
Yutaka Tachibana <yutaka.paradigm.shift@gmail.com>
2015-02-04 16:19:56 +0900

    Fix script for replSet.

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,14 +13,16 @@ _EOT_
   yum install -y mongodb-org-2.6.7 mongodb-org-server-2.6.7 mongodb-org-shell-2.6.7 mongodb-org-mongos-2.6.7 mongodb-org-tools-2.6.7
   echo "exclude=mongodb-org,mongodb-org-server,mongodb-org-shell,mongodb-org-mongos,mongodb-org-tools" | tee -a /etc/yum.conf
 
-  sed -i 's/127.0.0.1/ALL/' /etc/mongod.conf
+  sed -i 's/bind_ip=127.0.0.1/bind_ip=0.0.0.0/' /etc/mongod.conf
   sed -i 's/#replSet=setname/replSet=rs0/' /etc/mongod.conf
 
-  echo "export LC_ALL=en_US.UTF-8" >> ~/.bash_profile
+  echo "export LC_ALL=en_US.UTF-8" >> /home/vagrant/.bash_profile
   localedef  -c -i en_US -f UTF-8 en_US.UTF-8
 
   service mongod start
   chkconfig mongod on
+
+  mongo --eval 'rs.initiate()'
 fi
 
 if [ ! -e /etc/init.d/elasticsearch ] ; then
